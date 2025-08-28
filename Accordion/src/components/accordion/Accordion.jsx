f7c01bccdf7f285c161926265e2fe2375620a6a7
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-08-28 14:04:02 +0530

    add multi selection functionality  in accordion

--- Accordion/src/components/accordion/Accordion.jsx
+++ Accordion/src/components/accordion/Accordion.jsx
@@ -3,32 +3,64 @@
 import data from "./data";
 import "./accordion.css";
 
-import { useState } from "react";
+import { useActionState, useState } from "react";
 
 const accordion = () => {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelectoion] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
   const hanndleSingleSelection = (curid) => {
-    console.log(curid);
     setSelected(curid === selected ? null : curid);
   };
+  const handleMultiSelection = (curid) => {
+    let copyMul = [...multiple];
+    const findIndexOfCurId = copyMul.indexOf(curid);
+    if (findIndexOfCurId == -1) copyMul.push(curid);
+    else copyMul.splice(findIndexOfCurId, 1);
+    setMultiple(copyMul);
+  };
 
+  console.log(selected, multiple);
   return (
     <div className="wrapper ">
+      <button
+        onClick={() => {
+          setEnableMultiSelectoion(!enableMultiSelection);
+        }}
+      >
+        {enableMultiSelection
+          ? "Enable Multiselection"
+          : "Disable Multiselection"}
+      </button>
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item">
-              <div className="tittle"
-              onClick={() => hanndleSingleSelection(dataItem.id)}
+              <div
+                className="tittle"
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => hanndleSingleSelection(dataItem.id)
+                }
               >
                 <h3>{dataItem.question}</h3>
-                <span >
+                <span>
                   <button> {selected != dataItem.id ? "+" : "-"}</button>
                 </span>
               </div>
-              {selected === dataItem.id ? (
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="content"> {dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="content"> {dataItem.answer}</div>
+                  )}
+              {/* {selected === dataItem.id ||
+              multiple.indexOf(dataItem.id) !== -1 ? (
                 <div className="content">{dataItem.answer}</div>
-              ) : null}
+              ) : null} */}
             </div>
           ))
         ) : (
